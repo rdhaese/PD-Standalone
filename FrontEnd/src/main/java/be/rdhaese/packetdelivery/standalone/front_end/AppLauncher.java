1ccd68f1f7b0ec7b1d7dfa6f380c523ec59615dc
Robin D'Haese <robin.d'haese@realdolmen.com>
2015-12-30 10:23:42 +0100

    created AppLauncher and SpringFxmlLoader(custom loading of .fxml files) classes to be able to start the application

--- FrontEnd/src/main/java/be/rdhaese/packetdelivery/standalone/front_end/AppLauncher.java
+++ FrontEnd/src/main/java/be/rdhaese/packetdelivery/standalone/front_end/AppLauncher.java
@@ -1,9 +1,31 @@
 package be.rdhaese.packetdelivery.standalone.front_end;
 
+import javafx.application.Application;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * Created on 30/12/2015.
  *
  * @author Robin D'Haese
  */
-public class AppLauncher {
+@SpringBootApplication
+public class AppLauncher extends Application
+{
+    public static final SpringFxmlLoader LOADER = new SpringFxmlLoader();
+
+    public static void main( String[] args )
+    {
+        launch(args);
+    }
+    
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = (Parent) LOADER.load("login-form");
+        primaryStage.setScene(new Scene(root, 800, 800));
+        primaryStage.setTitle("PacketDelivery");
+        primaryStage.show();
+    }
 }
---
+++ FrontEnd/src/main/java/be/rdhaese/packetdelivery/standalone/front_end/SpringFxmlLoader.java
@@ -0,0 +1,25 @@
+package be.rdhaese.packetdelivery.standalone.front_end;
+
+import javafx.fxml.FXMLLoader;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class SpringFxmlLoader {
+
+    public static final ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppLauncher.class);
+
+    public Object load(String fxmlFileName) {
+        try (InputStream fxmlStream = SpringFxmlLoader.class
+                .getResource(String.format("/fxml/%s.fxml", fxmlFileName)).openStream()){
+            //TODO log entry
+            FXMLLoader loader = new FXMLLoader();
+            loader.setControllerFactory(applicationContext::getBean);
+            return loader.load(fxmlStream);
+        } catch (IOException ioException) {
+            throw new RuntimeException(ioException);
+        }
+    }
+}
